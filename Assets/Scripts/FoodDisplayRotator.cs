36f740f99d801105b03dc78c5f2be03875bebac9
Sumit-pan <pandey.sum123@gmail.com>
2026-04-23 14:54:53 -0700

    Add food zoom interaction

--- Assets/Scripts/FoodDisplayRotator.cs
+++ Assets/Scripts/FoodDisplayRotator.cs
@@ -8,9 +8,20 @@ public class FoodDisplayRotator : MonoBehaviour
     [SerializeField] private float interactionSpinDegrees = 360f;
     [SerializeField] private float interactionSpinSpeed = 300f;
     [SerializeField] private float swipeTriggerThreshold = 28f;
+    [SerializeField] private float pinchZoomSensitivity = 0.003f;
+    [SerializeField] private float mouseWheelZoomSensitivity = 0.2f;
+    [SerializeField] private float minScaleMultiplier = 0.75f;
+    [SerializeField] private float maxScaleMultiplier = 1.65f;
 
     private bool interactionEnabled = true;
     private float queuedSpinDegrees;
+    private Vector3 initialScale;
+    private float currentScaleMultiplier = 1f;
+
+    private void Awake()
+    {
+        initialScale = transform.localScale;
+    }
 
     public void Configure(Vector3 axis, float speed)
     {
@@ -43,6 +54,12 @@ public class FoodDisplayRotator : MonoBehaviour
         {
             transform.Rotate(rotationAxis, autoRotationSpeed * Time.deltaTime, Space.Self);
         }
+
+        if (TryGetPinchDelta(out float pinchDelta))
+            ApplyZoom(pinchDelta * pinchZoomSensitivity);
+
+        if (TryGetScrollDelta(out float scrollDelta))
+            ApplyZoom(scrollDelta * mouseWheelZoomSensitivity);
     }
 
     private void QueueInteractionSpin(float swipeDirection)
@@ -56,7 +73,7 @@ public class FoodDisplayRotator : MonoBehaviour
     private bool TryGetSwipeDirection(out float swipeDirection)
     {
         swipeDirection = 0f;
-        
+
         if (!interactionEnabled)
             return false;
 
@@ -81,6 +98,50 @@ public class FoodDisplayRotator : MonoBehaviour
         return false;
     }
 
+    private bool TryGetPinchDelta(out float pinchDelta)
+    {
+        pinchDelta = 0f;
+
+        if (!interactionEnabled || Input.touchCount < 2)
+            return false;
+
+        Touch firstTouch = Input.GetTouch(0);
+        Touch secondTouch = Input.GetTouch(1);
+
+        if (IsTouchOverUI(firstTouch.fingerId) || IsTouchOverUI(secondTouch.fingerId))
+            return false;
+
+        Vector2 firstPreviousPosition = firstTouch.position - firstTouch.deltaPosition;
+        Vector2 secondPreviousPosition = secondTouch.position - secondTouch.deltaPosition;
+
+        float previousDistance = Vector2.Distance(firstPreviousPosition, secondPreviousPosition);
+        float currentDistance = Vector2.Distance(firstTouch.position, secondTouch.position);
+
+        pinchDelta = currentDistance - previousDistance;
+        return Mathf.Abs(pinchDelta) > 0.01f;
+    }
+
+    private bool TryGetScrollDelta(out float scrollDelta)
+    {
+        scrollDelta = 0f;
+
+        if (!interactionEnabled || IsPointerOverUI())
+            return false;
+
+        scrollDelta = Input.mouseScrollDelta.y;
+        return Mathf.Abs(scrollDelta) > 0.001f;
+    }
+
+    private void ApplyZoom(float zoomDelta)
+    {
+        currentScaleMultiplier = Mathf.Clamp(
+            currentScaleMultiplier + zoomDelta,
+            minScaleMultiplier,
+            maxScaleMultiplier);
+
+        transform.localScale = initialScale * currentScaleMultiplier;
+    }
+
     private static bool IsTouchOverUI(int fingerId)
     {
         return EventSystem.current != null && EventSystem.current.IsPointerOverGameObject(fingerId);
@@ -96,6 +157,9 @@ public class FoodDisplayRotator : MonoBehaviour
         if (!interactionEnabled)
             return false;
 
+        if (Input.touchCount > 1)
+            return true;
+
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -103,6 +167,6 @@ public class FoodDisplayRotator : MonoBehaviour
                 return true;
         }
 
-        return Input.GetMouseButton(0) && !IsPointerOverUI();
+        return (Input.GetMouseButton(0) || Mathf.Abs(Input.mouseScrollDelta.y) > 0.001f) && !IsPointerOverUI();
     }
 }
